4ea6129a540c4297c7352860f1babf01e258688e
adarsh-g-s <adarsh431593@gmail.com>
2021-05-04 09:45:25 +0200

    Push changes

--- ImageToCartoon.py
+++ ImageToCartoon.py
@@ -1,19 +1,33 @@
 import cv2
 import numpy as np
-# import streamlit as st
+import streamlit as st
+from PIL import Image
+
 #
-# st.title("Stream")
+st.title("Web app to convert image to animated cartoon")
 
-# Read image
-uploadedImage = cv2.imread("C:\\Users\\agarakah\\Desktop\\26891940.jpg")
+# Request for image upload
+uploadImage = st.file_uploader("Please upload the image to convert to cartoon", type=["jpg","png"])
 
-# Convert to grayscale
-grayScaledImage = cv2.cvtColor(uploadedImage, cv2.COLOR_BGR2GRAY)
+'Check if file is empty'
+if uploadImage is None:
+    st.text("Image missing! Please upload")
+else:
+    # Read image
+    uploadedImage = Image.open(uploadImage)
+    # uploadedImage = cv2.imread("C:\\Users\\agarakah\\Desktop\\26891940.jpg")
+    st.image(uploadedImage, use_column_width=True)
+    # Convert image to numpy array
+    uploadedImage = np.asarray(uploadedImage)
 
-# Detect edges
+    # Convert to grayscale
+    grayScaledImage = cv2.cvtColor(uploadedImage, cv2.COLOR_BGR2GRAY)
 
-edgeDetectedImages = cv2.adaptiveThreshold(grayScaledImage, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 7)
+    # Detect edges
 
-# Display images
-cv2.imwrite("Cartoon.jpg",edgeDetectedImages)
+    edgeDetectedImages = cv2.adaptiveThreshold(grayScaledImage, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 7)
+
+    # Display images
+    # cv2.imwrite("Cartoon.jpg",edgeDetectedImages)
+    st.image(edgeDetectedImages, use_column_width=True)
 print()
---
+++ README.md
@@ -0,0 +1,13 @@
+# Image to Cartoon
+
+Python program that uses basic image filtering techniques to convert uploaded image to cartoon.
+
+The uploaded image is converted first to grayscale and then edges are detected in the uploaded image.
+The filtered image is then saved as the cartooon image.
+
+This app has been deployed using the popular streamlit[1] API
+
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/Adarsh-g-s/Image2Cartoon/Img2Cartoon/)
+
+### References
+[1] https://streamlit.io/
